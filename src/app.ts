c18c078f9fabd2cd36fc3dc9c5d72d8ea5b95471
AlxndrVgt <alexander.vogt@avolutions.de>
2024-12-03 13:03:54 +0100

    Add basic template with input files

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import * as fs from 'fs';
+import * as path from 'path';
+
+const filePath = path.resolve(__dirname, '../src/input.txt');
+
+// Read the file content
+fs.readFile(filePath, 'utf8', (err, data) => {
+
+});
