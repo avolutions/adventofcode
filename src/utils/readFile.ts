40eb148bd4245b469b34660ddab94b1259c7aed8
AlxndrVgt <alexander.vogt@avolutions.de>
2024-12-03 18:15:10 +0100

    Add global input file

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export function readFileFromCurrentDirectory(relativePath: string): string {
-  const srcBaseDir = path.resolve(__dirname, "../../src");
-  const filePath = path.join(srcBaseDir, relativePath);
+export function readFileFromCurrentDirectory(): string {
+  const srcBaseDir = path.resolve(__dirname, "../..");
+  const filePath = path.join(srcBaseDir, 'input.txt');
 
   if (!fs.existsSync(filePath)) {
     throw new Error(`File not found: ${filePath}`);
